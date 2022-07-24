70073671ec22b4c6ff9172885f2153a245bb0951
Ganeshan M <86967803+mganeshan29@users.noreply.github.com>
2022-07-24 11:52:26 +0530

    Time: 36 ms (99.62%), Space: 14.4 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,7 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        n = len(matrix[0])
-        def get(idx: int) -> int:
-            r, c = divmod(idx, n)
-            return matrix[r][c]
-        return get(bisect_left(range(len(matrix)*n-1), target, key=get)) == target
+        r = len(matrix)
+        if r == 0: return False
+        
+        c = len(matrix[0])
+        r1, r2 = 0, r-1
+        while r1 < r2:
+            mid = r1 + (r2-r1)//2
+            if matrix[mid][c-1] < target: r1 = mid+1
+            elif matrix[mid][0] > target: r2 = mid-1
+            else:
+                r1 = mid
+                break
+        
+        c1, c2 = 0, c-1;
+        while c1 <= c2:
+            mid = c1 + (c2-c1)//2
+            if matrix[r1][mid] < target: c1 = mid+1
+            elif matrix[r1][mid] > target: c2 = mid-1
+            else: return True
+        
+        return False
