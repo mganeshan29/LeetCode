fca661e2dd931f8d183ec02d6976338fd3fb4c6b
Ganeshan M <86967803+mganeshan29@users.noreply.github.com>
2022-07-28 19:02:10 +0530

    Time: 39 ms (98.58%), Space: 14.4 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,15 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         s1=sorted(s)
+#         s2=sorted(t)
+#         if s1==s2:
+#             return True
+#         else:
+#             return False
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s1=sorted(s)
-        s2=sorted(t)
+        s1=Counter(s)
+        s2=Counter(t)
         if s1==s2:
             return True
         else:
